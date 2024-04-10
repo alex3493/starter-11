78e241f5a97fb8a4e9dd20e9491b2041c0299e2f
Alex <admin@smartcalc.org>
2024-04-10 18:20:47 +0200

    SecondaryButton.vue: add option to release focus after button click.

--- resources/js/Components/SecondaryButton.vue
+++ resources/js/Components/SecondaryButton.vue
@@ -1,19 +1,28 @@
 <script setup lang="ts">
-withDefaults(
+const props = withDefaults(
     defineProps<{
-        type?: 'button' | 'submit' | 'reset';
+      type?: 'button' | 'submit' | 'reset';
+      releaseFocus?: boolean
     }>(),
     {
-        type: 'button',
+      type: 'button',
+      releaseFocus: true
     }
 );
+
+const buttonClick = (ev: any) => {
+  if (props.releaseFocus) {
+    ev.target.blur()
+  }
+}
 </script>
 
 <template>
-    <button
-        :type="type"
-        class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
-    >
-        <slot />
-    </button>
+  <button
+      :type="type"
+      @click.prevent="buttonClick"
+      class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
+  >
+    <slot/>
+  </button>
 </template>
